f6b55d9bd389b3bfd9d30e464b79388107fd03c9
HenryCabrera <henrycabrera083@gmail.com>
2022-04-22 18:36:20 -0600

    Agregar archivos de proyecto.

---
+++ AplicaciondeRifa/Clases/Rifa.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicaciondeRifa.Clases
+{
+    internal class Rifa
+    {
+
+        public string[] premios = { "Lavadora", "Licuadora", "Vajilla de Ceramica", "Set de vasos" };
+
+
+        public int GeneraNumero(int n1, int n2)
+        {
+
+            Random r = new Random();
+            return r.Next(n1, n2);
+
+        }
+        public int GeneraPremio()
+        {
+            Random prix = new Random();
+            return prix.Next(1, 5);
+
+        }
+
+        public void rifa(string[] Concursantes, int Premios)
+        {
+            string premio, ganador;
+            premio = ganador = "";
+            int tombola = 0;
+            int i;
+            for (i = 1; i <= Premios; i++)
+            {
+                tombola = GeneraNumero(0, Concursantes.Length - 1);
+                Console.WriteLine($"Premio numero {i}: {premios[i - 1]}");
+                Console.WriteLine($"Ganador: {Concursantes[tombola]}");
+            }
+
+        }
+    }
+}
---
+++ AplicaciondeRifa/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AplicaciondeRifa.Clases;
+
+namespace AplicaciondeRifa
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Rifa casa = new Rifa();
+        public string[] concursantes = {"Henry", "Lesly", "Jorge", "Ángeles", "Geiser", "Zoe", "Omar",
+        "Juan", "Ana", "Marlon"};
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BttnEmpezar_Click(object sender, RoutedEventArgs e)
+        {
+            int premios = casa.GeneraPremio(); 
+            CantPremios.Text = Convert.ToString(casa.GeneraPremio());
+           casa.rifa(concursantes, casa.GeneraPremio());
+
+        }
+    }
+}
